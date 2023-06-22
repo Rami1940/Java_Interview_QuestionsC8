9c79cac39ef7f2acab1721dcc13a1aaf7a39e297
mansurucuoqlu <121526908+mansurucuoqlu@users.noreply.github.com>
2023-06-22 12:55:17 -0700

    Q3

--- JavaInterviewQuestions/ReverseString_mansur.java
+++ JavaInterviewQuestions/ReverseString_mansur.java
@@ -1,5 +1,30 @@
 package JavaInterviewQuestions;
 
 public class ReverseString_mansur {
+    public static void main(String[] args) {
+
+    }
+    public static String UsingStringBuilder(String p_Word){
+        StringBuilder SB = new StringBuilder(p_Word);
+        SB.reverse();
+        return SB.toString();
+    }
+
+    public static String RegularWay(String p_Word){
+        String ReversedString = "";
+        for (int i = 0; i < p_Word.length(); i++) {
+            ReversedString = p_Word.charAt(i) + ReversedString;
+        }
+        return ReversedString;
+    }
+
+    public static String RegularWayWithForEach(String p_Word){
+        char[] MyCharArray = p_Word.toCharArray();
+        String ReversedString = "";
+        for (char MyChar :MyCharArray){
+            ReversedString = MyChar + ReversedString;
+        }
+        return ReversedString;
+    }
 
 }
