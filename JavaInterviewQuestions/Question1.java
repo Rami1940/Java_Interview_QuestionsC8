b9f985f455c508d265503feb3d341cfcc714d97f
Roman0nyshchuk <126744790+Roman0nyshchuk@users.noreply.github.com>
2023-07-17 21:19:27 -0400

    gitconflict1

--- JavaInterviewQuestions/Question1.java
+++ JavaInterviewQuestions/Question1.java
@@ -5,9 +5,9 @@ public class Question1 {
     public static void main(String[] args) {
         System.out.println("This is to manage conflicts");
     }
-
+// this is a roman comment
     public static String RegularWay(String p_Word){
-        String ReversedString = "Real Madrid2";
+        String ReversedString = "Barcelona";
         for (int i = 0; i < p_Word.length(); i++) {
             ReversedString = p_Word.charAt(i) + ReversedString;
         }
--- JavaInterviewQuestions/Question2.java
+++ JavaInterviewQuestions/Question2.java
@@ -1,4 +1,9 @@
 package JavaInterviewQuestions;
 
 public class Question2 {
+    public static String UsingStringBuilder(String p_Word){
+        StringBuilder SB = new StringBuilder(p_Word);
+        SB.reverse();
+        return SB.toString();
+    }
 }
