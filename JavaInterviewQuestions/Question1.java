4bc5b7e3b725fb4f3e0af68828f5bcb11345d7d7
Roman0nyshchuk <126744790+Roman0nyshchuk@users.noreply.github.com>
2023-07-17 20:40:21 -0400

    gitconflict

--- JavaInterviewQuestions/Question1.java
+++ JavaInterviewQuestions/Question1.java
@@ -1,4 +1,11 @@
 package JavaInterviewQuestions;
 
 public class Question1 {
+    public static String RegularWay(String p_Word){
+        String ReversedString = "";
+        for (int i = 0; i < p_Word.length(); i++) {
+            ReversedString = p_Word.charAt(i) + ReversedString;
+        }
+        return ReversedString;
+    }
 }
